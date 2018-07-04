0ede6ee66441bfb17066dca4b245810d1f530696
blaze6950 <d.progect21@gmail.com>
2018-07-04 14:03:52 +0300

    Added into solution 5 dll files

---
+++ SecondClassLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondClassLibrary
+{
+    public class Class1
+    {
+    }
+}
