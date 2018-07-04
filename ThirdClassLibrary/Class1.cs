772acc547ddc8174cc52dd7d63d8f375bd93ec09
blaze6950 <d.progect21@gmail.com>
2018-07-04 14:45:42 +0300

    Implement IExtension interface at onemore dll class

--- ThirdClassLibrary/Class1.cs
+++ ThirdClassLibrary/Class1.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using ExampleOfUsingAppDomainAndDll;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ThirdClassLibrary
 {
-    public class Class1
+    public class Class1 : IExtension
     {
+        public string GetExtensionName()
+        {
+            return Assembly.GetEntryAssembly().GetName().Name;
+        }
     }
 }
