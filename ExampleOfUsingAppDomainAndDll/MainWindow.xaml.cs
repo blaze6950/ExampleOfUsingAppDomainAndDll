65df04cb0e8b0dd18d7e65314030d6a02fb06ada
blaze6950 <d.progect21@gmail.com>
2018-07-04 14:43:51 +0300

    Implement in one dll class interface IExtension and config GUI

--- ExampleOfUsingAppDomainAndDll/MainWindow.xaml.cs
+++ ExampleOfUsingAppDomainAndDll/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,12 +23,75 @@ namespace ExampleOfUsingAppDomainAndDll
     {
         public MainWindow()
         {
+            DllSateTextBlock.Text = "Unloaded";
             InitializeComponent();
+            FirstInitialize();
+            StateButton.IsEnabled = true;
+            StateButton.Content = "Unload";
+            DllSateTextBlock.Text = "Loaded";
+        }
+
+        private void FirstInitialize()
+        {
+            var res = MessageBox.Show("Load .dll files from default path?", "Load DLL", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            DllSateTextBlock.Text = "Loading...";
+            if (res == MessageBoxResult.Yes)
+            {
+                LoadDllFromDirectory();
+            }
+            else if (res == MessageBoxResult.No)
+            {                
+                LoadDllFromDirectory(ChooseDirectory());
+            }
+            else
+            {
+                throw new Exception("Some wents wrong... Pls,try restart the app, sorry");
+            }
+        }
+
+        private string ChooseDirectory()
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = false;
+            openFileDialog.ShowDialog();
+            return openFileDialog.SafeFileName;
+        }
+
+        private void LoadDllFromDirectory()
+        {
+            LoadDllFromDirectory(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
+        }
+
+        private void LoadDllFromDirectory(String path)
+        {
+            
         }
 
         private void StateButton_Click(object sender, RoutedEventArgs e)
         {
+            if (!((string)(StateButton.Content)).Contains("Unload"))
+            {
+                StateButton.IsEnabled = false;
+                StateButton.Content = "Unload";
+                DllSateTextBlock.Text = "Loading...";
+                FirstInitialize();
+                StateButton.IsEnabled = true;
+                DllSateTextBlock.Text = "Loaded";
+            }
+            else
+            {
+                StateButton.IsEnabled = false;
+                StateButton.Content = "Load";
+                DllSateTextBlock.Text = "Unloading...";
+                UnloadAllDll();
+                StateButton.IsEnabled = true;
+                DllSateTextBlock.Text = "Unloaded";
+            }
+        }
 
+        private void UnloadAllDll()
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- FifthClassLibrary/Class1.cs
+++ FifthClassLibrary/Class1.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using ExampleOfUsingAppDomainAndDll;
 
 namespace FifthClassLibrary
 {
-    public class Class1
+    public class Class1 : IExtension
     {
+        public string GetExtensionName()
+        {
+            return Assembly.GetEntryAssembly().GetName().Name;
+        }
     }
 }
